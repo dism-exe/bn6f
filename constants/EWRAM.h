9ecc0e8433322706b4a6ff8a50d4bae6170a8473
Alzakariya <alzakariyamq@msoe.edu>
2018-08-01 22:30:47 -0500

    save

--- constants/EWRAM.h
+++ constants/EWRAM.h
@@ -11,7 +11,7 @@ in the 0x2000000 RAM region
 #include "structs/NPC.h"
 #include "structs/reqBBS_GUI.h"
 
-#define timer_2000000 ((u32*) 0x2000000)
+#define timer_2000000 ((u32*)0x2000000)
 #define reqBBS_requestEntries_list ((u8*)0x02000290)
 #define reqBBS_bxo_2001150 ((u8*)0x02001150)
 #define gameState ((GameState*)0x2001B80)
@@ -34,15 +34,15 @@ in the 0x2000000 RAM region
 /*
   Chaud (ACDC), Subship seller guy
 */
-#define npc_2005F48 ((NPC*) 0x2005F48) // DEAD: no xrefs. Last @ 2005810
+#define npc_2005F48 ((NPC*)0x2005F48) // DEAD: no xrefs. Last @ 2005810
 /*
   Note from the past: those are sorta weird
   TODO: check this and see what's up with it
 */
-#define pCoords ((u32*)0x02009F60)
+#define pCoords ((u32*)0x2009F60)
 /*
   Always in r10
 */
-#define toolkit ((Toolkit*) 0x20093B0)
+#define toolkit ((Toolkit*)0x20093B0)
 
 #endif // EWRAM_H
