510337c044edd310b26fbe2c6ac7786a92024f67
Alzakariya <alzakariyamq@msoe.edu>
2018-08-01 20:47:41 -0500

    add EWRAM.h

---
+++ constants/EWRAM.h
@@ -0,0 +1,10 @@
+/**
+This file contains a memory map define macros for fixed data/structs
+in the 0x2000000 RAM region
+*/
+#ifndef EWRAM_H
+#define EWRAM_H
+
+#define toolkit ((Toolkit*)0x20093B0)
+
+#endif // EWRAM_H
