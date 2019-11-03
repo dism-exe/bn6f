9edef10f4f769ee4de82d3d6785d96dfd70dbb00
Lan <lanhikarixx@gmail.com>
2019-11-03 16:18:48 -0600

    rename compScripts_credits_86C4B58.lz77 to .lz

--- data.s
+++ data.s
@@ -615,7 +615,7 @@ comp_86C45A8::
 dat38_63::
 	.include "data/dat38_63.s"
 compScripts_credits_86C4B58::
-	.incbin "data/scripts/compScripts_credits_86C4B58.lz77"
+	.incbin "data/scripts/compScripts_credits_86C4B58.lz"
 dat38_64::
 	.include "data/dat38_64.s"
 comp_86C5190::
